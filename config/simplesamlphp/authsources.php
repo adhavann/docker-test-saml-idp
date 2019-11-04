eaf1fb559895276d7756d66345c6e7cc52284c67
Adhavann Ramalingam <aadhavan007@gmail.com>
2019-11-04 16:46:35 -0500

    Static

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -7,17 +7,7 @@ $config = array(
     ),
 
     'example-userpass' => array(
-        'exampleauth:UserPass',
-        'user1:user1pass' => array(
-            'uid' => array('1'),
-            'eduPersonAffiliation' => array('group1'),
-            'email' => 'user1@example.com',
-        ),
-        'user2:user2pass' => array(
-            'uid' => array('2'),
-            'eduPersonAffiliation' => array('group2'),
-            'email' => 'user2@example.com',
-        ),
+        'exampleauth:Static'
     ),
 
 );
